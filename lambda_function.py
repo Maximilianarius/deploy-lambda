68a88a8ea90b26796340205695017ecae871795f
Maximilianarius <MFlorea@lomixtech.com>
2020-08-03 21:06:49 -0400

    my first cicd pipeline

---
+++ lambda_function.py
@@ -0,0 +1,13 @@
+  
+import json
+
+
+def lambda_handler(event, context):
+    print(json.dumps(event))
+    return {
+        'status': 200,
+        'headers': {
+            'Content-Type': 'application/json'
+        },
+        'body': json.dumps({'message': 'Hello-OK'})
+    }
